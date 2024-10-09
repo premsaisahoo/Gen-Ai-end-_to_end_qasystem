25f4aa325eed5a60f1da22c1c3f783fd7ea235f6
premsaisahoo <premsaisahoo@gmail..com>
2024-10-09 17:15:29 +0530

    final commit with successful in streamlit lunch and output

--- QAWithPDF/embedding.py
+++ QAWithPDF/embedding.py
@@ -5,10 +5,22 @@ from llama_index.embeddings.gemini import GeminiEmbedding
 
 from QAWithPDF.data_ingestion import load_data
 from QAWithPDF.model_api import load_model
+from llama_index.core import Settings
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.llms.gemini import Gemini
+from llama_index.core import ServiceContext
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 
 import sys
 from exception import customexception
 from logger import logging
+
+
+google_api_key = os.getenv("GOOGLE_API_KEY")
 
 def download_gemini_embedding(model,document):
     """
@@ -20,10 +32,18 @@ def download_gemini_embedding(model,document):
     try:
         logging.info("")
         gemini_embed_model = GeminiEmbedding(model_name="models/embedding-001")
-        service_context = ServiceContext.from_defaults(llm=model,embed_model=gemini_embed_model, chunk_size=800, chunk_overlap=20)
+        Settings.llm = Gemini(models = "gemini-pro",api_key = google_api_key)
+        Settings.embed_model = GeminiEmbedding(model="models/embedding-001")
+        Settings.node_parser = SentenceSplitter(chunk_size=800, chunk_overlap=20)
+
+       
+
+        #service_context = ServiceContext.from_defaults(llm=model,embed_model=gemini_embed_model, chunk_size=800, chunk_overlap=20)
         
         logging.info("")
-        index = VectorStoreIndex.from_documents(document,service_context=service_context)
+        index = VectorStoreIndex.from_documents(document, embed_model=gemini_embed_model)
+
+        #index = VectorStoreIndex.from_documents(document,service_context=service_context)
         index.storage_context.persist()
         
         logging.info("")
